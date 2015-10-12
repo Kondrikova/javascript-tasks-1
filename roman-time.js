b3bf509e1832e1887495135cabe2ca869a10691f
Kondrikova <kondrikova12@gmail.com>
2015-10-12 19:08:13 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,28 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
-// Немного замечательного кода и магии
+var units = ['','I','II','III','IV','V','VI','VII','VIII','IX'];
+var tens = ['','X','XX','XXX','XL','L'];
+
+function getUnits(time) {
+  var unit = time%10;
+  return units[unit];
+}
+
+function getTens(time) {
+  var ten = Math.floor(time/10);
+  return tens[ten];
+}
+
+function toRoman(hours, minutes) {
+  var result ='';9
+  result = getTens(hours) + getUnits(hours) +' : '+getTens(minutes)+getUnits(minutes);
+  return result;
+}
+
+if ((hours>=0) && (hours<=23) && (minutes>=0) && (minutes<=59)){
+  console.log(toRoman(hours,minutes));
+}
+else {
+  console.log('Время указано неверно');
+}
